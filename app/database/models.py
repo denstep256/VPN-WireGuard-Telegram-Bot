68e9d276e703ceb4850c7d3ad2de503adec0ce81
denstep256 <stepanenko-200@bk.ru>
2024-09-19 01:10:03 +0300

    db

--- app/database/models.py
+++ app/database/models.py
@@ -49,7 +49,6 @@ class Subscribers(Base):
     expiry_date: Mapped[str] = mapped_column(String(25))
     notif_oneday = mapped_column(Boolean, default=False)
     note: Mapped[str | None] = mapped_column(String, nullable=True, default="")
-    subscripti: Mapped[str] = mapped_column(String(25))
 
 class TestPeriod(Base):
     __tablename__ = 'test_period'
